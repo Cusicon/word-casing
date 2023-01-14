606a5fd859f907854f65f049dd02774f57c82450
Success Chukwu <iamcusicon@gmail.com>
2023-01-14 16:18:15 +0100

    resolve the toSendteseCase function

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 // Convert string into human name like
 String.prototype.toNameCase = function () {
   let word = this.valueOf()
-  let result = ""
+  let result = ''
 
   word = word.toLowerCase().trim()
-  if (word.includes(" ")) {
+  if (word.includes(' ')) {
     let wordArr = []
-    let words = word.split(" ")
+    let words = word.split(' ')
     words.forEach((word) => {
-      wordArr.push(
-        word.replace(word.charAt(0), word.charAt(0).toUpperCase())
-      )
+      wordArr.push(word.replace(word.charAt(0), word.charAt(0).toUpperCase()))
     })
 
-    return (wordArr = wordArr.join(" "))
+    return (wordArr = wordArr.join(' '))
   }
   result = word.replace(word.charAt(0), word.charAt(0).toUpperCase())
   return result
@@ -22,19 +20,19 @@ String.prototype.toNameCase = function () {
 // Convert string into sentence like
 String.prototype.toSentenceCase = function () {
   let word = this.valueOf()
-  let result = ""
+  let result = ''
 
-  word = word.toLowerCase().trim()
-  if (word.includes(".")) {
+  word = word.trim()
+  if (word.includes('.')) {
     let wordArr = []
-    let words = word.split(" ")
+    let words = word.split('.')
     words.forEach((word) => {
-      wordArr.push(
-        word.replace(word.charAt(0), word.charAt(0).toUpperCase())
-      )
+      word = word.trim()
+      const firstLetter = word.charAt(0)
+      wordArr.push(word.replace(firstLetter, firstLetter.toUpperCase()))
     })
 
-    return (wordArr = wordArr.join(" "))
+    return (wordArr = wordArr.join('. '))
   }
   result = word.replace(word.charAt(0), word.charAt(0).toUpperCase())
   return result
